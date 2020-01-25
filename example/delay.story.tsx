222e367089224e7ee956a7d57e6bde4e259f628d
kimcoder <kimcoder@me.com>
2020-01-25 16:52:49 +0900

    add gh-pages, storybook ci/cd  (#1)
    * [feature/gh-pages] modify storybook cli
    
    * [feature/gh-pages] modify target branch
    
    * [feature/gh-pages] modify storybook config
    
    * [feature/gh-pages] modify cli
    
    * [feature/gh-pages] modify github-actions env
    
    * [feature/gh-pages] modify storybook config path
    
    * [feature/gh-pages] remove example ignore
    
    * [feature/gh-pages] modify path
    
    * [feature/gh-pages] modify gitignored
    
    * [feature/gh-pages] update readme
    
    * [feature/gh-pages] fix github-action target branch

---
+++ example/delay.story.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import SequencePlayer from '../src/SequencePlayer';
+import SampleImage from './sample.png';
+
+storiesOf('SequencePlayer', module)
+  .add('custom delay, 300', () => {
+    const playerRef = React.useRef<any>(null);
+
+    const handleControl = (action: string) => () => {
+      switch (action) {
+        case 'play': playerRef.current.play(); break;
+        case 'pause': playerRef.current.pause(); break;
+        case 'resume': playerRef.current.resume(); break;
+        case 'reverse': playerRef.current.reverse(); break;
+      }
+    };
+
+    return (
+      <div>
+        <button onClick={handleControl('play')}>PLAY</button>
+        <button onClick={handleControl('pause')}>PAUSE</button>
+        <button onClick={handleControl('resume')}>RESUME</button>
+        <button onClick={handleControl('reverse')}>REVERSE PLAY</button>
+        <br/><br/>
+        <SequencePlayer
+          image={SampleImage}
+          data={[
+            { x: 0, y: 0 },
+            { x: -183, y: 0 },
+            { x: -366, y: 0 },
+            { x: -545, y: 0 },
+            { x: -732, y: 0 },
+            { x: 0, y: -172 },
+            { x: -183, y: -172 },
+            { x: -366, y: -172 },
+            { x: -549, y: -172 },
+            { x: -732, y: -172 },
+            { x: 0, y: -338 },
+            { x: -183, y: -339 },
+            { x: -363, y: -338 },
+            { x: -549, y: -338 }
+          ]}
+          playerSize={{ width: 183, height: 168 }}
+          imageSize={{ width: 918, height: 506 }}
+          ref={playerRef}
+          delay={200}
+        />
+      </div>
+    );
+  });
